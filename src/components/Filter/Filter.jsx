5b9ab8a20fbfcb00bd6da70cf31dad3eb6b92bca
Aryanshhandelwal-systango <aryansh.khandelwal@systango.com>
2024-05-27 19:05:37 +0530

    product_listing and styling

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,43 +1,52 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './Filter.css';
-function Filter() {
 
-    const [selectedOption, setSelectedOption] = useState('Option 1');
+const Filter = () => {
+  const [selectedOption, setSelectedOption] = useState('Option 1');
 
-    const handleChange = (event) => {
-      setSelectedOption(event.target.value);
-    };
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
+
+  // Arrays for filters and sort options
+  const filters = ['T-shirt', 'Denim', 'Sweatshirts', 'Polo T-shirts'];
+  const sortOptions = [
+    { value: 'Option 1', label: 'Sort by: Price Low to High' },
+    { value: 'Option 2', label: 'Sort by: Price High to Low' },
+    { value: 'Option 3', label: 'Sort by: Popularity' },
+    { value: 'Option 4', label: 'Sort by: Newest' }
+  ];
+
   return (
     <>
       {/*main-page-content*/}
       <span className='small-text'>
-            <p className='small-text'>Home / Clothing / Mens Clothing / All Mens Clothing</p>
-          </span>
-   
-          <span className='details'>
-            <p className='details'><b>All Products</b> (25 Products) </p>
-          </span>
+        <p className='small-text'>Home / Clothing / Mens Clothing / All Mens Clothing</p>
+      </span>
+
+      <span className='details'>
+        <p className='details'><b>All Products</b> (25 Products) </p>
+      </span>
+
+      <span className="filter_sort">
+        <span className='filter'>
+          <h4>Filters:</h4>
          
-          <span className="filter_sort">
-          <span className='filter'>
-            <h4>Filters:</h4>
-            <button className='filter_buttons'>T-shirt</button>
-            <button className='filter_buttons'>Denim</button>
-            <button className='filter_buttons'>Sweatshirts</button>
-            <button className='filter_buttons'>Polo T-shirts</button>
-          </span>
-          <span>
-          <select className='sort' value={selectedOption} onChange={handleChange} >
-            <option value="Option 1">Sort by: Price Low to High</option>
-            <option value="Option 2">Sort by: Price High to Low</option>
-            <option value="Option 3">Sort by: Popularity</option>
-            <option value="Option 4">Sort by: Newest</option>
+          {filters.map((filter, index) => (
+            <button key={index} className='filter_buttons'>{filter}</button>
+          ))}
+        </span>
+        <span>
+          <select className='sort' value={selectedOption} onChange={handleChange}>
+            {sortOptions.map((option) => (
+              <option key={option.value} value={option.value}>{option.label}</option>
+            ))}
           </select>
-          </span>
-          </span>
-         
+        </span>
+      </span>
     </>
   );
-}
+};
 
 export default Filter;
+
--- src/components/Product/product.jsx
+++ src/components/Product/product.jsx
@@ -1,17 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import './Product.css';
+import {AiFillCloseCircle} from 'react-icons/ai';
 import ProductData from './productData';
 
 
-function product() {
+function Product() {
+  const [detail, setDetail] = useState([]);
+  const[close,setClose] = useState(false);
+  const detailPage = (Product) => {
+    setDetail([{...Product}])
+    setClose(true)
+  }
+  const size = ['XS','S', 'M', 'L', 'XL'];
   return (
     <>
+    {
+      close ?
+      <div className="detail_container">
+      <div className="detail_content">
+        <button className="close" onClick={() => setClose(false)}><AiFillCloseCircle/></button>
+        {
+          detail.map((x)=>{
+            return (
+              <div key = {x.id}>
+                <div className="detail_info">
+                  <div className="img-box">
+                    <img src={x.img} alt={x.title} ></img>
+                  </div>
+                  <div className="detail">
+                    <h2>{x.title}</h2>
+                    <h3>{x.content}</h3>
+                    <h4>Size: {size.map((size, index) => (
+            <button key={index} className='size_buttons'>{size}</button>
+          ))}</h4>
+                    <h3>$ {x.price}</h3>
+                    <p>{x.price_Content}</p>
+                    <button>Add to Cart</button>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+      </div>
+    </div>: null
+    }
+ 
       <div className="product_container">
         {
             ProductData.map((shirt) =>{
                 return(
                     <>
-                    <div className="product_box">
+                    <div className="product_box" onClick={() =>detailPage(shirt)}>
                         <div className="product_content">
                             <div className="img-box">
                                 <img src={shirt.img} alt={shirt.title}></img>
@@ -19,8 +59,8 @@ function product() {
                             <div className="product_detail">
                                 <div className="info">
                                     <h3>{shirt.title}</h3>
-                                    <p>{shirt.content}</p>
-                                    <p><b>{shirt.price}</b></p>
+                                    <p><b>Size:</b> {shirt.size}</p>
+                                    <p><b>${shirt.price}</b></p>
                                 </div>
                                 <button>View</button>
                             </div>
@@ -34,4 +74,4 @@ function product() {
   )
 }
 
-export default product;
+export default Product;
