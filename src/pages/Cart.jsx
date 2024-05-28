00bfa83fddc85bddb5fbf2fa67d06603cbeb496e
Aryanshhandelwal-systango <aryansh.khandelwal@systango.com>
2024-05-28 12:27:44 +0530

    one cart page update

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Helmet from '../components/Helmet/Helmet';
+
+function Cart() {
+  return (
+    <Helmet title= {'Cart'}>
+
+
+   </Helmet>
+  );
+}
+
+export default Cart;
